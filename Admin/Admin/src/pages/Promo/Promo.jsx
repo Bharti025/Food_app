eac7a05894ff8fcf884fd8e2449c39ec6ebd9909
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-15 22:44:04 +0530

    Update Promo.jsx

--- Admin/Admin/src/pages/Promo/Promo.jsx
+++ Admin/Admin/src/pages/Promo/Promo.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 function Promo() {
-  const url = "http://localhost:4000";
+  const url = "https://food-app-backend-l63s.onrender.com";
 
   const [data, setDate] = useState({
     code: "",
