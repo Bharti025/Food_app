55bf216c8b8e34552b6d92b3575312b7e474cd27
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-15 21:58:07 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://bhartiwadhwa025_db_user:dtwbKrgqvZJKYXcc@cluster0.0iasrxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    process.env.MONGO_URL
     )
     .then(() => console.log("DB Connected"));
 };
